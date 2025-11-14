39b4d8e8154ecd3b3129c542a446ea33ee0ff34f
juliscapucin <jscapucin@gmail.com>
2025-11-14 13:46:21 +0000

    add gradient to menu-mobile

--- src/components/ui/menuMobile.tsx
+++ src/components/ui/menuMobile.tsx
@@ -36,10 +36,18 @@ export default function MenuMobile({ navLinks }: NavLinksProps) {
 	useGSAP(
 		() => {
 			if (!mobileMenuRef.current) return
+			gsap.set(mobileMenuRef.current, { y: window.innerHeight - 70 })
+		},
+		{ dependencies: [], scope: mobileMenuRef }
+	)
+
+	useGSAP(
+		() => {
+			if (!mobileMenuRef.current) return
 
 			if (isMenuOpen) {
 				gsap.to(mobileMenuRef.current, {
-					yPercent: 0,
+					y: 0,
 					duration: 0.5,
 					ease: 'power3.out',
 					onComplete: () => {
@@ -50,7 +58,7 @@ export default function MenuMobile({ navLinks }: NavLinksProps) {
 			} else {
 				document.body.style.overflow = 'unset'
 				gsap.to(mobileMenuRef.current, {
-					yPercent: 92,
+					y: window.innerHeight - 70,
 					duration: 0.5,
 					ease: 'power3.in',
 				})
@@ -77,73 +85,78 @@ export default function MenuMobile({ navLinks }: NavLinksProps) {
 			<header className='fixed inset-0 z-100 block h-dvh md:hidden gutter-stable pointer-events-none'>
 				{/* EXPANDED MENU */}
 				<aside
-					className='h-svh w-full bg-primary p-6 pointer-events-auto'
+					className='relative h-svh w-full p-[22px] pt-0 pointer-events-auto bg-primary'
 					ref={mobileMenuRef}
 					role='dialog'
 					aria-modal='true'
 					aria-labelledby='mobile-menu-title'>
-					{/* LOGO */}
-					<div className='absolute top-6 left-6 right-6 flex items-start justify-between z-100'>
-						<Logo isDescriptionVisible={isMenuOpen} />
-						{/* BURGER BUTTON */}
-						<ButtonBurger
-							className={`md:hidden ${isMenuOpen ? 'opacity-0' : 'opacity-100 pointer-events-auto'} transition-opacity duration-300`}
-							onClick={() => toggleMenu()}
-							aria-expanded={isMenuOpen}
-							aria-controls='mobile-menu'
-							aria-haspopup='dialog'
-							aria-label={'Open navigation menu'}
-						/>
+					<div className='absolute -top-[3px] h-1 w-full bg-linear-to-b from-transparent to-primary'></div>
+					<div className='w-full h-full'>
+						{/* LOGO */}
+						<div className='absolute top-6 left-6 right-6 flex items-start justify-between z-100'>
+							<Logo isDescriptionVisible={isMenuOpen} />
+							{/* BURGER BUTTON */}
+							<ButtonBurger
+								className={`md:hidden ${isMenuOpen ? 'opacity-0' : 'opacity-100 delay-200'} transition-opacity duration-300`}
+								onClick={() => toggleMenu()}
+								aria-expanded={isMenuOpen}
+								aria-controls='mobile-menu'
+								aria-haspopup='dialog'
+								aria-label={'Open navigation menu'}
+							/>
+						</div>
+
+						{/* CLOSE BUTTON */}
+						<div
+							className={`absolute bottom-6 right-6 z-100 flex items-end justify-between bg-primary ${isMenuOpen ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
+							<ButtonClose
+								aria-label='Close navigation menu'
+								onClick={() => toggleMenu()}
+							/>
+						</div>
+
+						{/* SCREEN READER TITLE */}
+						<h2 id='mobile-menu-title' className='sr-only'>
+							Navigation Menu
+						</h2>
+
+						{/* NAV LINKS */}
+						<nav
+							aria-label='Navigation'
+							className='flex h-full items-end pb-40'>
+							<ul className='space-y-6'>
+								{navLinks.map((link) => {
+									const isCurrentPage =
+										(link.slug === '/' && pathname === '/') ||
+										(link.slug !== '/' && pathname.includes(`${link.slug}`))
+
+									return (
+										<li key={link.slug}>
+											<Link
+												href={link.slug}
+												className='relative flex items-center'
+												onClick={(e) => {
+													e.preventDefault()
+													toggleMenu(link.slug)
+												}}
+												aria-current={isCurrentPage ? 'location' : undefined}
+												tabIndex={isMenuOpen ? 0 : -1}>
+												{/* BULLET */}
+												<span
+													ref={bulletRef}
+													className={`absolute h-2 w-2 bg-secondary rounded-full transition-transform duration-300 ${pathname === `/${link.slug}` ? 'scale-100' : 'scale-0'}`}
+													aria-hidden='true'></span>
+												<span
+													className={`text-secondary transition-transform duration-500 ${isCurrentPage ? 'translate-x-4 font-bold' : 'translate-0'}`}>
+													{link.label}
+												</span>
+											</Link>
+										</li>
+									)
+								})}
+							</ul>
+						</nav>
 					</div>
-
-					{/* CLOSE BUTTON */}
-					<div
-						className={`absolute bottom-6 right-6 z-100 flex items-end justify-between bg-primary ${isMenuOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0'} transition-opacity duration-300`}>
-						<ButtonClose
-							aria-label='Close navigation menu'
-							onClick={() => toggleMenu()}
-						/>
-					</div>
-
-					{/* SCREEN READER TITLE */}
-					<h2 id='mobile-menu-title' className='sr-only'>
-						Navigation Menu
-					</h2>
-
-					{/* NAV LINKS */}
-					<nav aria-label='Navigation' className='flex h-full items-end pb-40'>
-						<ul className='space-y-6'>
-							{navLinks.map((link) => {
-								const isCurrentPage =
-									(link.slug === '/' && pathname === '/') ||
-									(link.slug !== '/' && pathname.includes(`${link.slug}`))
-
-								return (
-									<li key={link.slug}>
-										<Link
-											href={link.slug}
-											className='relative flex items-center'
-											onClick={(e) => {
-												e.preventDefault()
-												toggleMenu(link.slug)
-											}}
-											aria-current={isCurrentPage ? 'location' : undefined}
-											tabIndex={isMenuOpen ? 0 : -1}>
-											{/* BULLET */}
-											<span
-												ref={bulletRef}
-												className={`absolute h-2 w-2 bg-secondary rounded-full transition-transform duration-300 ${pathname === `/${link.slug}` ? 'scale-100' : 'scale-0'}`}
-												aria-hidden='true'></span>
-											<span
-												className={`text-secondary transition-transform duration-500 ${isCurrentPage ? 'translate-x-4 font-bold' : 'translate-0'}`}>
-												{link.label}
-											</span>
-										</Link>
-									</li>
-								)
-							})}
-						</ul>
-					</nav>
 				</aside>
 			</header>
 		)
